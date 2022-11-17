857b3dc4a59cd9ee5ea67e5a3dcc150498fb54ff
LrScripting <118460256+LrScripting@users.noreply.github.com>
2022-11-17 21:53:10 +0000

    Create spinwords.js

---
+++ spinwords.js
@@ -0,0 +1,31 @@
+/* Description:
+
+Write a function that takes in a string of one or more words, and returns the same string, but with all five or more letter words reversed (Just like the name of this Kata). Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
+
+Examples:
+
+spinWords( "Hey fellow warriors" ) => returns "Hey wollef sroirraw" 
+spinWords( "This is a test") => returns "This is a test" 
+spinWords( "This is another test" )=> returns "This is rehtona test"
+
+*/
+function spinWords(string){
+
+    let array = string.split(" ")
+
+    for(let i = 0; i < array.length; i++){
+        let kek = array[i].split("")
+        if(kek.length >= 5){
+            let reversedElement = kek.reverse()
+            let reversedString = reversedElement.join('')   
+            array[i] = reversedString; 
+        }
+        if (array.length == 1){
+            let array = string.split("")
+            var reverse = array.reverse();
+            var reversestring = reverse.join('')
+            return reversestring;
+          }   
+    }
+    return array.join(' ')
+  }
